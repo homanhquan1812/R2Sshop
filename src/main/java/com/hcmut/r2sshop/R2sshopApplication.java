161ee89f06869ea0355733eed6e04382fb2c0bd4
Hồ Mạnh Quân <homanhquan1812@gmail.com>
2024-08-02 09:35:11 +0700

    Initial Update

---
+++ src/main/java/com/hcmut/r2sshop/R2sshopApplication.java
@@ -0,0 +1,13 @@
+package com.hcmut.r2sshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2sshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R2sshopApplication.class, args);
+	}
+
+}
