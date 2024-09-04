db4c3e6629ba598e661987b6665c6b7e562d1e0e
Hồ Mạnh Quân <homanhquan1812@gmail.com>
2024-09-04 22:02:03 +0700

    Removed MERN Stack project

--- MERN Stack/backend/src/route/register.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const registerController = require('../app/controller/RegisterController.js')
-
-router.post('/', registerController.register)
-
-module.exports = router
